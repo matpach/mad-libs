85f2abec435ea31ae3169b15e5af6f60b34fe02c
Mateo Veron <100798617+matpach@users.noreply.github.com>
2022-08-02 19:19:17 -0300

    Add files via upload

---
+++ mad libs/mid-labs.py
@@ -0,0 +1,10 @@
+
+color = input("enter a color: ")
+plural_noun = input("enter a prular noun: ")
+celebrity = input("enter a celebrity: ")
+
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+
